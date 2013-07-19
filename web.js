48b9d8246cf937185a72ca57dfc57f679e630384
Mike Kelly <mikechriskelly@gmail.com>
2013-07-19 17:54:29 +0800

    Added title and photos

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
-var express = require('express');
+var express = require('express')
+  , path = require('path');
 
 var app = express.createServer(express.logger());
 
@@ -8,7 +9,10 @@ app.get('/', function(request, response) {
     response.send(fs.readFileSync('./index.html').toString('utf-8'));
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
